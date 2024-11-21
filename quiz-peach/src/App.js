9e8c20b5b0922390fcfe72a422b6cac7f2bbb6df
s.soltanian <s.soltaniant@gmail.com>
2024-11-21 18:36:48 +0330

    add empty Theme

--- quiz-peach/src/App.js
+++ quiz-peach/src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +23,7 @@ function App() {
         </a>
       </header>
     </div>
+    </ThemeProvider>
   );
 }
 
