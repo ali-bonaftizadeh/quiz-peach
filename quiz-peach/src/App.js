def59fa376af41f88ed87aba0e90fc2bb418cdba
s.soltanian <s.soltaniant@gmail.com>
2024-11-21 22:45:36 +0330

    add simple two column layout

--- quiz-peach/src/App.js
+++ quiz-peach/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Box, CssBaseline } from '@mui/material';
+import { Box, Container, CssBaseline, Grid2, Typography } from '@mui/material';
 import FiltersBox from './components/FiltersBox'
 import ToggleBox from './components/ToggleBox';
 
@@ -15,9 +15,21 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className="App">
-        <ToggleBox title="سوالات من"></ToggleBox>
-        <Box sx={{m:5}}></Box> {/* only for test*/}
-        <FiltersBox title="درجه سختی" options={["ساده", "دشوار", "متوسط"]}></FiltersBox>
+        <Container>
+          <ToggleBox title="سوالات من"></ToggleBox>
+          <Box sx={{ m: 5 }}></Box> {/* only for test*/}
+          <FiltersBox title="درجه سختی" options={["ساده", "دشوار", "متوسط"]}></FiltersBox>
+
+          <Grid2 container spacing={2}>
+            <Grid2 size={{sm:12, md:3}}>
+              {/* Right Narrow Sidebar */}
+            </Grid2>
+            <Grid2 size={{sm:12, md:9}}>
+              {/* Main Content Div */}
+            </Grid2>
+          </Grid2>
+        </Container>
+
       </div>
     </ThemeProvider>
   );
