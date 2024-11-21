efc2930c8a91e54e440d0cd2bed96c50f58ca2f1
s.soltanian <s.soltaniant@gmail.com>
2024-11-21 21:57:57 +0330

    use vazirmatn font

--- quiz-peach/src/App.js
+++ quiz-peach/src/App.js
@@ -1,28 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 
-const theme = createTheme();
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Vazirmatn'
+  }
+});
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <CssBaseline />
+      <div className="App">
+      </div>
     </ThemeProvider>
   );
 }
