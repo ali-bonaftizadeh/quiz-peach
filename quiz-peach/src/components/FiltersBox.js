fa369170ae97595d884201d78736082d54bf1398
s.soltanian <s.soltaniant@gmail.com>
2024-11-21 22:34:44 +0330

    add FiltersBox

---
+++ quiz-peach/src/components/FiltersBox.js
@@ -0,0 +1,42 @@
+import { Box, Card, Stack, Typography, FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+import Divider from '@mui/material/Divider';
+
+const FiltersBox = ({title, options}) => {
+    return (
+        <Card>
+            <Stack>
+        <Box sx={{
+                display: 'flex',
+                px: 2,
+                py: 2,
+                cursor: 'pointer',
+            }}
+            >
+            <Typography>{title}</Typography>
+        </Box>
+        <Divider />
+        <Box sx={{
+                display: 'flex',
+                py: 1,
+                cursor: 'pointer',
+            }}
+            >
+            <FormGroup>
+            {
+                options.map(option => {
+                    return (
+                        <FormControlLabel sx={{m:0}} control={<Checkbox/>} label={option} />
+                    )
+                })
+            }
+        </FormGroup>
+        </Box>
+        </Stack>
+
+        </Card >
+
+    )
+}
+
+
+export default FiltersBox;
