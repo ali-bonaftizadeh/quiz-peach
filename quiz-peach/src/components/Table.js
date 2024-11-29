60cc6a8c79080cbbb1e0d22b708fcced93532c27
s.soltanian <s.soltaniant@gmail.com>
2024-11-29 19:34:48 +0330

    add Table component

---
+++ quiz-peach/src/components/Table.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from '@mui/material';
+
+export default function BasicTable({titles, rows}) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+            {
+                titles.map(title => {
+                    return (<TableCell key={title} align='right'>{title}</TableCell>);
+                })
+            }
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => {
+            console.log(rows);
+            console.log(row);
+
+            return (
+              <TableRow
+                key={row.key}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                 {row.columns.map(c => (<TableCell key={c} align="right">{c}</TableCell>))}
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
