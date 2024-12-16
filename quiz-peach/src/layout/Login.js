6446674752d9230f61f6d25a0af5700e9e21815a
s.soltanian <s.soltaniant@gmail.com>
2024-12-16 11:21:30 +0330

    route to main page after signup

--- quiz-peach/src/layout/Login.js
+++ quiz-peach/src/layout/Login.js
@@ -60,6 +60,7 @@ const Login = () => {
 
       const response = await postData('/user', { name, email, password });
       setMessage('ثبت‌نام با موفقیت انجام شد.');
+      routeChange("/");
     } catch (err) {
       setError('خطایی در ثبت‌نام رخ داد. لطفا از تکراری نبودن نام کاربری اطمینان حاصل کنید.');
     }
